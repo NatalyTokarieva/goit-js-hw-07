c09f537fe9ed5bd196e7d1bc771c1740c3a0dfea
Nataliia Tokarieva <tokareva_nata@ukr.net>
2022-08-07 23:09:27 +0200

    change 01,02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,18 +27,8 @@ function createItemsMarkup(galleryItems){
   .join("");
 
 }
-// divGallery.addEventListener('click', onImgGallery);
-// function onImgGallery(event){
-//     event.preventDefault();
 
-// if(event.target.nodename !== 'IMG'){
-//     return;
-// }
-// const selectedImg = event.target.dataset.source;
-// selectedShow(selectedImg)
-// }
 
-// function selectedShow(images){
 
 divGallery.addEventListener('click', onImgGallery);
 function onImgGallery(event){
@@ -49,60 +39,19 @@ function onImgGallery(event){
     const selectedImg = event.target.dataset.source;
     selectedShow(selectedImg)
 }
-function selectedShow(images){
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`)}
-
-
-instance.show()
-
-
-// const gallery = galleryItems
-
-
-// const divGallery = document.querySelector('div.gallery')
-
-// const markup = gallery.map(({preview, original, description}) => {
-//     return `
-//     <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source=""${original}
-//       alt="${description}"
-//     />
-//   </a>
-// </div>`
-// }).join(' ');
-
-// divGallery.insertAdjacentHTML('beforeend', markup)
-// divGallery.addEventListener('click', onImgGallery);
-
-
-
-// function onImgGallery(event){
-//     event.preventDefault();
-
-// if(event.target.nodename !== 'IMG'){
-//     return;
-// }
-// const selectedImg = event.target.dataset.source;
-// selectedShow(selectedImg)
-// }
-
-// function selectedShow(images){
-
-
-
-//     const instance = basicLightbox.create(images)
-
-//     document.querySelector('images.create').onclick = instance.show
-    
-
-// }
+// const instance = basicLightbox.create(`
+//     <img src="${selectedImg}" width="800" height="600">
+// `)
 // instance.show()
+
+
+function selectedShow(selectedImg){
+    const instance = basicLightbox.create(`
+    <img src="${selectedImg}" width="800" height="600">
+`)
+instance.show()
+}
+
 
 
 
